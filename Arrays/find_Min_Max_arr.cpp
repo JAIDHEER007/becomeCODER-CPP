468cae44363da1023d8c6b7c40c1c16b90dc524b
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-05-24 08:56:44 +0530

    Find the MIN and MAX element in an array

---
+++ Arrays/find_Min_Max_arr.cpp
@@ -0,0 +1,31 @@
+//Finding minimum and maximum element in a given array
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findMin(vector<int> &arr){
+	int min = INT_MAX;
+	for(int element:arr)
+		if(element < min){ min = element; }
+	return min;
+}
+
+int findMax(vector<int> &arr){
+	int max = INT_MIN;
+	for(int element:arr)
+		if(element > max){ max = element; }
+	return max;
+}
+
+int main(){
+	vector<int> arr{ 35, 394, -129, 341, 
+					 -151, 126, 342, 8, 
+					 -145, 389, -125, 249,
+					 -135, 2, -170, 310  };
+					 
+	cout<<"Min element: "<<findMin(arr)<<endl;
+	cout<<"Max element: "<<findMax(arr)<<endl;
+	
+	return 0;
+}
