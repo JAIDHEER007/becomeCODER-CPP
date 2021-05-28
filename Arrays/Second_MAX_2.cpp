955b881d5acf967612b7239e8025dacf1c0edca7
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-05-28 09:14:40 +0530

    Find the second max using search and ignore operation

---
+++ Arrays/Second_MAX_2.cpp
@@ -0,0 +1,33 @@
+//Finding second max using search and ignore operations
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int secondMAX(vector<int> &arr){
+	int firstMAX, secondMAX;
+	firstMAX = secondMAX = INT_MIN;
+	
+	//finding the first max
+	for(int element:arr)
+		if(element > firstMAX){ firstMAX = element; }
+	
+	//finding the second max
+	for(int element:arr)
+		if((element!=firstMAX) && (element > secondMAX)){ secondMAX = element; }
+	
+	return secondMAX;
+}
+
+int main(){
+	vector<int> arr{ 5, 12, 6 };
+	cout<<secondMAX(arr)<<endl;
+	
+	vector<int> arr2{-4, -146, 128, 89, -178,
+					 -56, -170, -101, 31, -61 };
+					 
+	cout<<secondMAX(arr2)<<endl;
+	
+	return 0;
+}
