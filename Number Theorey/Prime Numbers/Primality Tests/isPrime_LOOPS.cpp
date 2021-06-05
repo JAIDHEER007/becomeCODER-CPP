83f7a477ff3eff843ac9d9b7fa2b225a13afcccb
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-06-05 14:54:48 +0530

    (6k ± 1) method with LOOP

--- Number Theorey/Prime Numbers/Primality Tests/isPrime_LOOPS.cpp
+++ Number Theorey/Prime Numbers/Primality Tests/isPrime_LOOPS.cpp
@@ -11,7 +11,7 @@ bool isPrime(long n){
 		return true;
 	if((n % 2 == 0) || (n % 3 == 0))
 		return false;
-	for(long long i=5;(i*i)<=n;i = i + 6){
+	for(long long i=5;(i*i)<=n;i+=6){
 		if((n % i == 0) || (n % (i+2) == 0))
 			return false;
 	}
