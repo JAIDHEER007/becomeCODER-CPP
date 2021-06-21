2abde43bb8d2812b5455df7ce91d552d46c18c41
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-06-21 22:09:27 +0530

    LC1154 Day of the Year || https://bit.ly/3qegTsp

--- Miscellaneous/LC1154_Day_Of_The_Year.cpp
+++ Miscellaneous/LC1154_Day_Of_The_Year.cpp
@@ -25,11 +25,27 @@ int dayOfYear(string date){
 	return (Cmonths[month-1]-(months[month-1]-day))+(((isLeapYear(year))&&(month>2))?(1):(0));
 }
 
+int dayOfYear2(string date){
+	int year = stoi(date.substr(0,4));
+	int month = stoi(date.substr(5,2));
+	int day = stoi(date.substr(8,2));
+	
+	int Cmonths[] = {0,31,59,90,120,151,181,212,243,273,304,334,365};
+	
+	return (Cmonths[month-1] + day)+(((isLeapYear(year))&&(month>2))?(1):(0));
+}
+
 int main(){
 	cout<<dayOfYear("2019-01-09")<<endl;
 	cout<<dayOfYear("2003-03-01")<<endl;
 	cout<<dayOfYear("2004-03-01")<<endl;
 	cout<<dayOfYear("2008-10-10")<<endl;
+	
+	cout<<dayOfYear2("2019-01-09")<<endl;
+	cout<<dayOfYear2("2003-03-01")<<endl;
+	cout<<dayOfYear2("2004-03-01")<<endl;
+	cout<<dayOfYear2("2008-10-10")<<endl;
+	
 	return 0;
 }
 
