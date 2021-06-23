d8eb5c7a6f43f94fc1e982631bff63e281666a3c
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-06-23 18:07:43 +0530

    Prime Factorization using TRAIL DIVISION method

--- Number Theorey/Prime Numbers/Prime Factorization/Prime_Factorization_TRAIL_DIVISION.cpp
+++ Number Theorey/Prime Numbers/Prime Factorization/Prime_Factorization_TRAIL_DIVISION.cpp
@@ -19,7 +19,7 @@ table primeFactorize(int number){
 	}
 	//From This point We eleiminated all 2,3 prime factors
 	//Now the prime factors will start form 5,7,11,13....
-	//Now 6k +- 1 Rule can be applied 
+	//Now 6k ± 1 Rule can be applied 
 	
 	for(int i=5;(i*i)<=number;i+=6){
 		while(!(number % i)){
